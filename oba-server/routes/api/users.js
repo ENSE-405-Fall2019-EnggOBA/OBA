6a1e0857c363ff5bd783d535ff904a1c20b1db09
jonathonflorek <jonathonflorek@gmail.com>
2019-10-25 21:34:00 -0600

    add user creation

--- oba-server/routes/api/users.js
+++ oba-server/routes/api/users.js
@@ -1,4 +1,6 @@
+const mongoose = require('mongoose')
 const router = require('express').Router()
+const User = mongoose.model('User')
 
 router.post('/users/login', (req, res, next) => {
     let errors = undefined
@@ -18,6 +20,18 @@ router.post('/users/login', (req, res, next) => {
         return res.status(422).json({ errors })
     }
 
+})
+
+router.post('/users', (req, res, next) => {
+    const user = new User()
+    user.email = req.body.user.email
+    user.setPassword(req.body.user.password)
+    
+    user.save().then(() => {
+        return res.json({
+            user: user.toAuthJSON()
+        })
+    }).catch(next)
 })
 
 module.exports = router
