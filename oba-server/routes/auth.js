e82c5736876217358bc346e1bf1f2e56d53a27ca
jonathonflorek <jonathonflorek@gmail.com>
2019-10-25 21:49:48 -0600

    auth maybe

---
+++ oba-server/routes/auth.js
@@ -0,0 +1,17 @@
+const jwt = require('express-jwt')
+const { secret } = require('../config')
+
+function getToken(req) {
+    const { authorization } = req.headers
+    const [ method, token ] = authorization.split(' ')
+    if ([ 'Token', 'Bearer' ].includes(method)) {
+        return token
+    }
+    return null
+}
+
+module.exports.required = jwt({
+    secret,
+    userProperty: 'payload',
+    getToken,
+})
