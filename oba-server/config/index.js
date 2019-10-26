3fca0af75b0a171886b95662a6532443b8cf87f8
jonathonflorek <jonathonflorek@gmail.com>
2019-10-25 21:54:46 -0600

    enhance config

--- oba-server/config/index.js
+++ oba-server/config/index.js
@@ -1,3 +1,4 @@
-module.exports = {
-    secret: 'secret',
-}
+module.exports.isProduction = process.env.NODE_ENV === 'production'
+module.exports.secret = 'secret'
+module.exports.mongodbUri = process.env.MONGODB_URI
+module.exports.port = process.env.PORT || 3000
