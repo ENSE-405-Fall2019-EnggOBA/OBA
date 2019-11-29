4590d4dfe9ecf8df3438939af044b5ea1929700f
jonathonflorek <jonathonflorek@gmail.com>
2019-11-28 23:39:23 -0600

    work on generating GA form

--- oba-client/addclass.js
+++ oba-client/addclass.js
@@ -54,7 +54,12 @@ $(document).ready(function() {
     }
 })
 
-function setupAttributesAndIndicators(attributeSelect, indicatorSelect) {
+function setupAttributesAndIndicators(
+    attributeSelect,
+    indicatorSelect,
+    attributeValue,
+    indicatorValue,
+) {
     // by default, indicator select is disabled
     indicatorSelect.prop('disabled', true)
 
@@ -77,6 +82,13 @@ function setupAttributesAndIndicators(attributeSelect, indicatorSelect) {
             indicatorSelect.prop('disabled', true)
         }
     })
+
+    if (attributeValue) {
+        attributeSelect.value = attributeValue
+    }
+    if (indicatorValue) {
+        indicatorSelect.value = indicatorValue
+    }
 }
 
 function createNew() {
@@ -94,10 +106,80 @@ function loadForId(id, token) {
     })
 }
 
-function buildForm() {
-
+function addGa(div, data) {
+    const id = div.prop('id')
+    div.append(`
+<section class="py-0">
+    <div class="col-sm">
+        <div class="form-group">
+            <select class="form-control" id="${id}.attribute"></select>
+        </div>
+    </div>
+    <div class="col-sm">
+        <div class="form-group">
+            <select class="form-control" id="${id}.indicator"></select>
+        </div>
+    </div>
+    <table class="table table-bordered">
+        <thead class="thead dark">
+            <tr>
+				<th scope="col">Fails to meet expectations</th>
+				<th scope="col">Developing</th>
+				<th scope="col">Meets Expectations</th>
+				<th scope="col">Exceeds Expectations</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr class="table-primary">
+                <td><input type="text" id="${id}.fail.criteria"/></td>
+                <td><input type="text" id="${id}.developing.criteria"/></td>
+                <td><input type="text" id="${id}.meets.criteria"/></td>
+                <td><input type="text" id="${id}.exceeds.criteria"/></td>
+            </tr>
+            <tr class="table-success">
+                <td><input type="number" id="${id}.fail.grade"/></td>
+                <td><input type="number" id="${id}.developing.grade"/></td>
+                <td><input type="number" id="${id}.meets.grade"/></td>
+                <td><input type="number" id="${id}.exceeds.grade"/></td>
+            </tr>
+            <tr class="table-warning>
+                <td><div class="custom-file">
+                    <input type="file" class="custom-file-input" id="${id}.fail.document">
+                    <label class="custom-file-label" for="${id}.fail.document">Add Document</label>
+                </div></td>
+                <td><div class="custom-file">
+                    <input type="file" class="custom-file-input" id="${id}.developing.document">
+                    <label class="custom-file-label" for="${id}.developing.document">Add Document</label>
+                </div></td>
+                <td><div class="custom-file">
+                    <input type="file" class="custom-file-input" id="${id}.meets.document">
+                    <label class="custom-file-label" for="${id}.meets.document">Add Document</label>
+                </div></td>
+                <td><div class="custom-file">
+                    <input type="file" class="custom-file-input" id="${id}.exceeds.document">
+                    <label class="custom-file-label" for="${id}.exceeds.document">Add Document</label>
+                </div></td>
+            </tr>
+        </tbody>
+    </table>
+</section>
+    `)
+    setupAttributesAndIndicators(
+        $('#' + id + '.attribute'),
+        $('#' + id + '.indicator'),
+        data.grad_attribute,
+        data.indicator,
+    )
+    const report = data.evaluation_report
+    if (report) {
+        ['fail', 'developing', 'meets', 'exceeds'].forEach(key => {
+            $(`#${id}.${key}.criteria`).value = report[key].criteria
+            $(`#${id}.${key}.grade`).value = report[key].grad_attribute
+            if (report[key].documents) {
+                $(`#${id}.${key}.document`).prop('disabled', true)
+            }
+        })
+    }
 }
 
-function submitForm() {
 
-}
