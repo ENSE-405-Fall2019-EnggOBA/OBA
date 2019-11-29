f7777fc8e86071023564987384237ca78cb0d84e
jonathonflorek <jonathonflorek@gmail.com>
2019-11-28 19:53:13 -0600

    extract functions to js file

---
+++ oba-client/addclass.js
@@ -0,0 +1,11 @@
+$('#back-btn').on('click', function() {
+    location.href='myclasses.html'
+})
+
+$('#add-ga-btn').on('click', function() {
+    alert("added GA!");
+})
+
+$('#save-btn').on('click', function() {
+    alert("Successfully submitted!");
+})
