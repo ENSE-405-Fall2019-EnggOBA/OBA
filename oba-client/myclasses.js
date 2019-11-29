37778dbfa689713d5dbd8dd1597cd54f6af62709
jonathonflorek <jonathonflorek@gmail.com>
2019-11-28 19:29:28 -0600

    table populated with data

--- oba-client/myclasses.js
+++ oba-client/myclasses.js
@@ -8,13 +8,25 @@ $(document).ready(function() {
         }, 500);
         return
     }
-    // const table = $('my-classes-tbody')
+    const table = $('#my-classes-tbody')
     $.ajax({
         type: 'GET',
         url: baseUrl + '/courses/all',
         headers: { 'Authorization': 'Bearer ' + token },
         success: ({ result }) => {
-            alert(`success: ${JSON.stringify(result)}`)
+            result.success.forEach(element => {
+                table.append($(`
+<tr>
+    <>FACULTY NAME</>
+    <>${element.faculty_name} ${element.course_number}</>
+    <>TERM</>
+    <>YEAR</>
+    <>${element.status}</>
+</tr>                
+                `).on('click', () => {
+                    alert('navigating to: ' + element)
+                }))
+            })
         },
         error: (e) => {
             window.localStorage.removeItem('oba-token')
